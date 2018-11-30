618403e788c50dc0d8bf22dede1e6fe39ae88750
mcastorena <miguel.angel.castorena@gmail.com>
2018-11-29 20:03:48 -0800

    symbolTableEntry and SCTNode Class added

---
+++ SCTNode.java
@@ -0,0 +1,38 @@
+package interpreter;
+
+import java.util.LinkedList;
+
+public class SCTNode {
+	private LinkedList<SCTNode> collection = new LinkedList<SCTNode>();
+	private SCTNode momNode;
+	public int currentChild = 0, totalChildren = 0, height;
+	public LinkedList<symbolTableEntry> symbolTable = new LinkedList<symbolTableEntry>();
+	
+	public SCTNode() {
+		
+	}
+	
+	
+	public void addChild(SCTNode child){
+		collection.add(child);
+	}
+	
+	public LinkedList<SCTNode> getChildren(){
+		return collection;
+	}
+	
+	public SCTNode getMom(){
+		return momNode;
+	}
+	
+	public void setMom(SCTNode newMom){
+		momNode = newMom;
+	}
+	
+	
+	
+	
+	public void addSymTabEntry(symbolTableEntry newEntry) {
+		symbolTable.add(newEntry);
+	}
+}
