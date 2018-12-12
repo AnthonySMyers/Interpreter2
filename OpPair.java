1a17beae3b2beeaa9388d2b74d6a97dedb796f35
AnthonySMyers <33797691+AnthonySMyers@users.noreply.github.com>
2018-12-11 20:00:03 -0800

    Create OpPair.java

---
+++ OpPair.java
@@ -0,0 +1,12 @@
+
+public class OpPair {
+	public int index1 = -1, index2 = -1;
+	public String operation;
+	
+	public OpPair(int first, int second, String op){
+		index1 = first;
+		index2 = second;
+		operation = op;
+	}
+
+}
