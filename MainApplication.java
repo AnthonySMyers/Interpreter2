8a084f8a1e1ea0b19b826f2e1e460a3e95325665
AnthonySMyers <33797691+AnthonySMyers@users.noreply.github.com>
2018-12-11 20:09:47 -0800

    Update MainApplication.java

--- MainApplication.java
+++ MainApplication.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 public class MainApplication {
 	public static void main(String args[]) throws Exception{
 		Scanner in = new Scanner(System.in);
-		Parser llp = new Parser();
 		System.out.print("Enter the path of the file: ");
 		String fileName = in.nextLine();
 		File parseFile= new File(fileName);										//create file from specified file path
 		if(parseFile.exists()) {												//check if tokenFile exists
 			if(parseFile.isFile() && parseFile.canRead()) {						//check if tokenFile is a file that can be read
-				llp.readFile(parseFile);										//pass file to lexer
-				llp.resetRoot();
-				llp.printPST(llp.getRoot());
+				Interpreter inp = new Interpreter(parseFile);										//pass file to lexer
 			}else {
 				System.out.println("File cannot be read.");						//print error message
 			}
