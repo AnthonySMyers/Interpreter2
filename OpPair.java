85addf82a4a5944970ac95a644d64272080cb3a0
Khoa100 <minhkhoavu954@gmail.com>
2018-12-14 18:55:56 -0800

    documentation added

--- OpPair.java
+++ OpPair.java
@@ -1,8 +1,18 @@
-
+/**
+ * stores the math operations to be used to calculate a variable later in program
+ * @author Anthony Myers
+ *
+ */
 public class OpPair {
 	public int index1 = -1, index2 = -1;
 	public String operation;
 	
+	/**
+	 * constructor to do math statement
+	 * @param first - index of first number
+	 * @param second - index of second number
+	 * @param op - math operation
+	 */
 	public OpPair(int first, int second, String op){
 		index1 = first;
 		index2 = second;
